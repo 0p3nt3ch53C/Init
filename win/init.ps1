a3e1f7b7398ff3182e59e1525db044d073e17b2f
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-11-09 23:41:25 -0500

    Update to have status checks as needed after REST API calls

--- win/init.ps1
+++ win/init.ps1
@@ -19,19 +19,17 @@ if ($Vagrant_Base_URL_Response.StatusCode -eq "200" ) {
 
 $Newest_Vagrant_Version = ($Vagrant_Base_URL_Response.ParsedHtml.getElementsByTagName('a') | Select-Object nameprop)[1].nameprop
 
-# $Vagrant_Filename = (-join("vagrant_",$Newest_Vagrant_Version,"_x86_64.msi"))
 $Vagrant_Filename = (-join("vagrant_",$Newest_Vagrant_Version,"_windows_amd64.msi"))
 
 $ProgressPreference = 'SilentlyContinue' # Allows a faster download speed.
 Invoke-WebRequest -Uri (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"/",$Vagrant_Filename)) -outfile "$DownloadPath\$Vagrant_Filename"
-
 # As needed, check download status:  $Vagrant_Download_Status = $?
+
 $Vagrant_File_Hash = (Get-FileHash "$DownloadPath\$Vagrant_Filename").Hash
 
 $Vagrant_File_Hash_Filename = (-join("vagrant_",$Newest_Vagrant_Version,"_SHA256SUMS"))
 
 Invoke-WebRequest -Uri (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"\",$Vagrant_File_Hash_Filename)) -outfile "$DownloadPath\$Vagrant_File_Hash_Filename"
-
 # As needed, check download status: $Vagrant_File_Hash_Download_Status = $?
 
 if (Select-String -Path $DownloadPath\$Vagrant_File_Hash_Filename -Pattern $Vagrant_File_Hash){ Write-Host "File signature for $Vagrant_Filename verified."}
