bc87098f1be29d29179314b9e05b8f3d6a6b498e
0p3nt3ch53C <186238699+0p3nt3ch53C@users.noreply.github.com>
2025-11-11 22:17:47 -0500

    Conversion of API calls to function

--- win/init.ps1
+++ win/init.ps1
@@ -21,12 +21,17 @@ $Vagrant_Base_URL = "https://releases.hashicorp.com/vagrant/"
 
 function APICall {
     param (
-        [string]$url
+        [string]$url,
+        [string]$filepath = $false
     )
-    $response = Invoke-WebRequest -Uri $url
+    if ($filepath) {
+        $response = Invoke-WebRequest -Uri $url -outfile $filepath
+    } else {
+        $response = Invoke-WebRequest -Uri $url
+    }
     if ($response.StatusCode -eq "200" ) {
         return $response
-    }else {
+    } else {
         return $null
     }
 }
@@ -38,7 +43,7 @@ $Newest_Vagrant_Version = ($Vagrant_Base_URL_Response.ParsedHtml.getElementsByTa
 $Vagrant_Filename = (-join("vagrant_",$Newest_Vagrant_Version,"_windows_amd64.msi"))
 
 $ProgressPreference = 'SilentlyContinue' # Allows a faster download speed.
-Invoke-WebRequest -Uri (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"/",$Vagrant_Filename)) -outfile "$DownloadPath\$Vagrant_Filename"
+$Vagrant_Installer_URL_Response = APICall -url -Uri (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"/",$Vagrant_Filename)) -filepath "$DownloadPath\$Vagrant_Filename"
 # As needed, check download status:  $Vagrant_Download_Status = $?
 
 $Vagrant_File_Hash = (Get-FileHash "$DownloadPath\$Vagrant_Filename").Hash
@@ -46,7 +51,7 @@ $Vagrant_File_Hash = (Get-FileHash "$DownloadPath\$Vagrant_Filename").Hash
 $Vagrant_File_Hash_Filename = (-join("vagrant_",$Newest_Vagrant_Version,"_SHA256SUMS"))
 
 # Update to use APICall Function
-Invoke-WebRequest -Uri (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"\",$Vagrant_File_Hash_Filename)) -outfile "$DownloadPath\$Vagrant_File_Hash_Filename"
+$Vagrant_Hash_From_Source_URL_Response = APICall -url (-join($Vagrant_Base_URL,$Newest_Vagrant_Version,"\",$Vagrant_File_Hash_Filename)) -filepath "$DownloadPath\$Vagrant_File_Hash_Filename"
 # As needed, check download status: $Vagrant_File_Hash_Download_Status = $?
 
 if (Select-String -Path $DownloadPath\$Vagrant_File_Hash_Filename -Pattern $Vagrant_File_Hash){ Write-Host "File signature for $Vagrant_Filename verified."}
